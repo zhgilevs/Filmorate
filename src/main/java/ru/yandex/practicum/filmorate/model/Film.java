8002636c1b01d8a58801bf98d25e958b24136f47
szhgilev <zhgilev.s.v@yandex.ru>
2023-06-01 08:28:55 +0300

    optimize annotations and import

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,9 +2,9 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -15,10 +15,10 @@ public class Film {
 
     private int id;
     private int rate;
-    @NonNull
+    @NotBlank
     @Size(min = 1, max = 200, message = "Описание должно быть не более 200 символов и не менее 1 символа")
     private String description;
-    @NonNull
+    @NotNull
     @Positive
     private int duration;
     @NotBlank
