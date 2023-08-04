d163d6523f0123126d7546d832c098fd7a8910f1
Arsenii <yanovskyiarsenii@yandex.ru>
2023-08-04 18:53:27 +0300

    Event Storage Methods.

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Event;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
