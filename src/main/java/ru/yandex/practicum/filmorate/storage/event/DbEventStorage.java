296f73116d024abaf018385983804b914437673f
Arsenii <yanovskyiarsenii@yandex.ru>
2023-08-04 18:56:34 +0300

    Event Storage Methods.

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/event/DbEventStorage.java
@@ -0,0 +1,62 @@
+package ru.yandex.practicum.filmorate.storage.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.DatabaseException;
+import ru.yandex.practicum.filmorate.model.Event;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+@Component("DbEventStorage")
+@Primary
+@RequiredArgsConstructor
+public class DbEventStorage implements EventStorage{
+    private final JdbcOperations jdbcTemplate;
+    @Override
+    public void addEvent(Event event) {
+        event.setTimestamp(LocalDate.now());
+        String sqlQuery = "INSERT INTO feeds (userId, entityId, timestamp, eventType, operation) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sqlQuery, new String[]{"ID"});
+            stmt.setInt(1, event.getUserId());
+            stmt.setInt(2, event.getEventId());
+            stmt.setDate(4, Date.valueOf(event.getTimestamp()));
+            stmt.setString(5, event.getEventType());
+            stmt.setString(5, event.getOperation());
+            return stmt;
+        }, keyHolder);
+    }
+
+    @Override
+    public List<Event> getFeedByUserId(int userId) {
+        String sqlQuery = "SELECT * FROM feeds WHERE userId=? ORDER BY eventId DESC";
+
+        return jdbcTemplate.query(sqlQuery, (rs, rowNum) -> makeEvent(rs));
+    }
+
+    private Event makeEvent(ResultSet rs) {
+        try {
+            return Event.builder()
+                    .eventId(rs.getInt("ID"))
+                    .userId(rs.getInt("userId"))
+                    .entityId(rs.getInt("entityId"))
+                    .timestamp(rs.getDate("timestamp").toLocalDate())
+                    .eventType(rs.getString("eventType"))
+                    .operation(rs.getString("operation"))
+                    .build();
+        } catch (SQLException e) {
+            throw new DatabaseException("Ошибка получения пользователя");
+        }
+    }
+}
